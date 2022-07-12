17b482cd4d543cbe827921086293f0fdc3e87503
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-07-12 19:59:10 +0300

    run stylelint fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["stylelint-config-standard",
+            "stylelint-config-recommended-scss",
             "stylelint-config-rational-order",
             "stylelint-prettier/recommended"],
   plugins: ["stylelint-order", "stylelint-scss"],
